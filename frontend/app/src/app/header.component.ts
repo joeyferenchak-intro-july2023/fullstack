5dfc55c4e54b09834c001de157cb7649f3157f9e
Joey Ferenchak <joseph_ferenchak@progressive.com>
2023-07-17 16:20:23 -0400

    Add tailwind CSS.

--- frontend/app/src/app/header.component.ts
+++ frontend/app/src/app/header.component.ts
@@ -4,13 +4,15 @@ import { Component } from "@angular/core";
     standalone: true,
     selector: 'app-header',
     template: `
-    <header>
-        <h1>Intro to Programming</h1>
-        <p> Sample full stack app for class</p>
+    <header class="p-4 border-b-2 border-black">
+        <h1 class="text-4xl text-primary font-black">Intro to Programming</h1>
+        <p>Sample Full-Stack Application For The Training Class</p>
     </header>
+
     `
 })
 
 export class HeaderComponent {
-
+    
 }
+
