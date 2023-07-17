4ad0bfc86c2035a5c74859288ad9c5243984f013
Joey Ferenchak <joseph_ferenchak@progressive.com>
2023-07-17 14:24:18 -0400

    Initial

---
+++ frontend/app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Intro to Programming';
+  foobar = 'I love foobar!';
+}
